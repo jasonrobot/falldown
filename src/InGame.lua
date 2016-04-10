d0dd12bfe9466ed445f922749534d21d83bd3923
Jason Howell <Jason Howell>
2016-04-09 17:51:53 -0700

    added some comments, added score to the game over screen, prevented the player from falling off the bottom of the screen

--- src/InGame.lua
+++ src/InGame.lua
@@ -4,18 +4,31 @@ Signal = require('signal')
 local Timer = require('timer')
 local Camera = require('camera')
 local P = love.physics
+local Gamestate = require('gamestate')
+
+-- gamestates
+local gameOver = require('GameOver')
 
 local Player = require('Player')
 local Line = require('Line')
 
+--gamestate table
 local state = {}
 
+--physics objects
 local player
 local lines = {} -- table to hold all our physical objects
 local walls = {}
+--physics world
 local world
+--camera object
 local cam
+--camera y location
+local camY
+-- where to draw the next line
 local nextLineY
+local score = 0
+local x, y = love.window.getMode()
 
 function state:init()
    local x, y = love.window.getMode()
@@ -35,27 +48,58 @@ function state:init()
    player = Player.new(world)
    player.intentions = {}
 
-   Timer.every(1, function() self:insertNextLines() end)
-	       
-   cam = Camera(x/2, y/2)
-   nextLineY = y + 100
+   camY = y/2
+   cam = Camera(x/2, camY)
+   nextLineY = y
 end
 
 function state:update(dt)
    --player.body:setLinearVelocity(0, 200)
    world:update(dt)
    Timer.update(dt)
-   local x = love.window.getMode()
-   local _, y = player.body:getWorldCenter()
-   cam:lookAt(x/2, y)
 
-   walls.left.body:setPosition(-5, y)
-   walls.right.body:setPosition(x+5, y)
+   --update the camera's position
+   local lowerBound = 50
+   if player.body:getY() < camY + (y/2) - lowerBound then
+      -- player is behind the camera bottom
+      camY = camY + (75 * dt)
+   else
+      camY = camY + (player.body:getY() - (camY + (y/2) - lowerBound))
+   end
+   cam:lookAt(x/2, camY)
+      
+   --move the walls with the screen
+   walls.left.body:setPosition(-5, camY)
+   walls.right.body:setPosition(x+5, camY)
 
+   --add lines if needed
+   if nextLineY-50 < camY + (y/2) then
+      local a, b = Line.new(world, nextLineY)
+      nextLineY = nextLineY + 100
+      table.insert(lines, a)
+      table.insert(lines, b)
+   end
+
+   --remove lines that arent needed (this doesnt work)
+   for k, v in next, lines do
+      if v.body:getY() + v.shape:getRadius() < camY - (y/2) then
+	 lines[k] = nil
+	 --lets also give the player some score when this happens
+	 score = score + 10
+      end
+   end
+   
+   --do player input
    if player.intentions.right then
       player.body:applyForce(200, 0)
    elseif player.intentions.left then
       player.body:applyForce(-200, 0)
+   end
+
+   --check if you lose
+   if select(2, player.body:getWorldCenter()) + player.shape:getRadius() < camY - (y/2) then
+      gameOver:setScore(score)
+      Gamestate.switch(gameOver)
    end
 end
 
@@ -66,13 +110,15 @@ function state:draw()
    love.graphics.circle("fill", player.body:getX(), player.body:getY(), player.shape:getRadius())
 
    love.graphics.setColor(150, 150, 150)
+   local lineCount = 0
    for k, v in next, lines do
+      lineCount = lineCount + 1
       love.graphics.polygon("fill", v.body:getWorldPoints(v.shape:getPoints()))
    end
-
-   love.graphics.polygon("fill", walls.left.body:getWorldPoints(walls.left.shape:getPoints()))
-
+   
    cam:detach()
+   
+   love.graphics.print(score, 20, 20)
 end
 
 function state:keypressed(key)
@@ -90,14 +136,5 @@ function state:keyreleased(key)
       player.intentions.left = false
    end
 end
-
-function state:insertNextLines()
-   local x, y = love.window.getMode()
-   local a, b = Line.new(world, nextLineY)
-   nextLineY = nextLineY + 200
-   table.insert(lines, a)
-   table.insert(lines, b)
-end
-
 
 return state
